b469a3df9caa6c73eb3ca8b994fb1cfb5106c3d8
Bruno Zell <bruno.zzell@gmail.com>
2018-08-31 22:52:50 +0200

    minimum working example for passing the acme challenge

---
+++ src/WebApp/AcmeService.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Certes;
+using Certes.Acme;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace WebApp {
+    public class AcmeService : HostedService {
+        private readonly IApplicationLifetime applicationLifetime;
+        private readonly IMemoryCache memoryCache;
+
+        public AcmeService(IApplicationLifetime applicationLifetime, IMemoryCache memoryCache) {
+            this.applicationLifetime = applicationLifetime;
+            this.memoryCache = memoryCache;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken cancellationToken) {
+            try {
+                var acme = new AcmeContext(WellKnownServers.LetsEncryptStagingV2);
+                var account = await acme.NewAccount("bruno.zzell@gmail.com", true);
+
+                // Save the account key for later use
+                var pemKey = acme.AccountKey.ToPem();
+
+                var order = await acme.NewOrder(new[] { "02041c1c.ngrok.io" });
+
+                var authz = ( await order.Authorizations() ).First();
+                var httpChallenge = await authz.Http();
+                var keyAuthz = httpChallenge.KeyAuthz;
+
+                memoryCache.Set("token", httpChallenge.Token);
+                memoryCache.Set("response", keyAuthz);
+
+                var x = await httpChallenge.Validate();
+
+                await Task.Delay(TimeSpan.FromSeconds(1));
+
+                var challenge = await authz.Http();
+                var challengeRessource = await challenge.Resource();
+
+                var challengeResult = await challenge.Validate();
+
+
+                x = await httpChallenge.Validate();
+                var privateKey = KeyFactory.NewKey(KeyAlgorithm.ES256);
+                var cert = await order.Generate(new CsrInfo {
+                    CountryName = "DE",
+                    State = "Ontario",
+                    Locality = "Toronto",
+                    Organization = "Certes",
+                    OrganizationUnit = "Dev",
+                    CommonName = "02041c1c.ngrok.io",
+                }, privateKey);
+
+                var certPem = cert.ToPem();
+
+                // OR
+
+                var pfxBuilder = cert.ToPfx(privateKey);
+                var pfx = pfxBuilder.Build("my-cert", "abcd1234");
+
+                applicationLifetime.StopApplication();
+            }
+            catch (Exception ex) {
+
+                throw;
+            }
+        }
+    }
+}
--- src/WebApp/Startup.Acme.cs
+++ src/WebApp/Startup.Acme.cs
@@ -1,16 +1,41 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using System;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace WebApp {
     public class StartupAcme {
-        public void ConfigureServices(IServiceCollection services) { }
+        public void ConfigureServices(IServiceCollection services) {
+            services.AddMemoryCache();
+            services.AddSingleton<IHostedService, AcmeService>();
+        }
 
-        public void Configure(IApplicationBuilder app, IApplicationLifetime lifetime) {
-            app.Run(async (context) => {
-                await context.Response.WriteAsync("Please fulfill the ACME challenge!");
-                lifetime.StopApplication();
+        public void Configure(IApplicationBuilder app, IMemoryCache memoryCache) {
+            app.Map("/.well-known/acme-challenge", mapped => {
+                app.Run(async context => {
+                    var token = context.Request.Path.ToString().Replace(".well-known/acme-challenge", "").Trim('/');
+                    if (!memoryCache.TryGetValue("token", out string expectedToken) ||
+                        !memoryCache.TryGetValue("response", out string responseKey)) {
+                        await context.Response.WriteAsync("No ACME challenge requested yet");
+                        return;
+                    }
+
+                    if (expectedToken?.Equals(token, StringComparison.InvariantCultureIgnoreCase) != true) {
+                        await context.Response.WriteAsync("No valid token transmitted");
+                        return;
+                    }
+
+                    context.Response.ContentLength = responseKey.Length;
+                    context.Response.ContentType = "application/octet-stream";
+                    await context.Response.WriteAsync(responseKey, context.RequestAborted);
+                });
+            });
+
+            app.Run(async context => {
+                await context.Response.WriteAsync("Please fulfill the ACME challenge by requesting /.well-known/acme-challenge/{token}");
             });
         }
     }
