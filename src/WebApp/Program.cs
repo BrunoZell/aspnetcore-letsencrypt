324d82f54a0391293485f369890ba7a2ccfbe2b5
Bruno Zell <bruno.zzell@gmail.com>
2018-08-31 21:07:06 +0200

    bootstrapped startup

--- src/WebApp/Program.cs
+++ src/WebApp/Program.cs
@@ -3,10 +3,20 @@ using Microsoft.AspNetCore.Hosting;
 
 namespace WebApp {
     public class Program {
-        public static void Main(string[] args)
-            => CreateWebHostBuilder(args)
+        public static void Main(string[] args) {
+            CreateAcmeHostBuilder()
                 .Build()
                 .Run();
+
+            CreateWebHostBuilder(args)
+                .Build()
+                .Run();
+        }
+
+        public static IWebHostBuilder CreateAcmeHostBuilder()
+           => new WebHostBuilder()
+                .UseKestrel(options => options.ListenAnyIP(80))
+                .UseStartup<StartupAcme>();
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args)
             => WebHost.CreateDefaultBuilder(args)
---
+++ src/WebApp/Startup.Acme.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace WebApp {
+    public class StartupAcme {
+        public void ConfigureServices(IServiceCollection services) { }
+
+        public void Configure(IApplicationBuilder app, IApplicationLifetime lifetime) {
+            app.Run(async (context) => {
+                await context.Response.WriteAsync("Please fulfill the ACME challenge!");
+                lifetime.StopApplication();
+            });
+        }
+    }
+}
