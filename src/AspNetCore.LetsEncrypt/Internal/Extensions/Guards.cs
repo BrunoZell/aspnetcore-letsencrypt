48adfb510cd109ef53a526520e7f4abefd1b1ed8
Bruno Zell <bruno.zzell@gmail.com>
2018-09-02 22:33:47 +0200

    check configured options

---
+++ src/AspNetCore.LetsEncrypt/Internal/Extensions/Guards.cs
@@ -0,0 +1,17 @@
+﻿using AspNetCore.LetsEncrypt.Exceptions;
+using System;
+
+namespace AspNetCore.LetsEncrypt.Internal.Extensions {
+    internal static class Guards {
+        public static T ArgNotNull<T>(this T argument, string argumentName = null) where T : class =>
+            argument ?? throw new ArgumentNullException(argumentName);
+
+        public static T OptionNotNull<T>(this T option, string optionName) where T : class =>
+            option ?? throw new LetsEncryptException($"{optionName} is a required option.");
+
+        public static string OptionNotBlank(this string option, string optionName) =>
+            String.IsNullOrWhiteSpace(option) ?
+                throw new LetsEncryptException($"{optionName} is a required option.") :
+                option;
+    }
+}
--- src/AspNetCore.LetsEncrypt/LetsEncryptBuilder.cs
+++ src/AspNetCore.LetsEncrypt/LetsEncryptBuilder.cs
@@ -1,4 +1,6 @@
-﻿using AspNetCore.LetsEncrypt.Options;
+﻿using AspNetCore.LetsEncrypt.Exceptions;
+using AspNetCore.LetsEncrypt.Internal.Extensions;
+using AspNetCore.LetsEncrypt.Options;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using System;
@@ -13,7 +15,6 @@ namespace AspNetCore.LetsEncrypt {
 
         // Todo: Pass (user defined) certificate store
         // Todo: Pass (user defined) auth-key store
-        // Todo: Validate some required options
 
         public LetsEncryptBuilder WithOptions(Action<LetsEncryptOptions> configureAction)
         {
@@ -48,20 +49,49 @@ namespace AspNetCore.LetsEncrypt {
         public LetsEncrypt Build()
         {
             if (_configurationSection == null && _configureAction == null) {
-                throw new Exception($"Lets Encrypt is not configured. Configure by invoking either {nameof(LetsEncryptBuilder.WithConfiguration)}() or {nameof(LetsEncryptBuilder.WithOptions)}().");
+                throw new LetsEncryptException($"Lets Encrypt is not configured. Configure by invoking either {nameof(LetsEncryptBuilder.WithConfiguration)}() or {nameof(LetsEncryptBuilder.WithOptions)}().");
             }
 
+            // Get options by (1) parse the passed configuration, if any...
             var options = _configurationSection != null ?
                 _configurationSection.Get<LetsEncryptOptions>() :
                 new LetsEncryptOptions();
 
+            // ... and (2) overwrite some options with the configure delegate, if any.
             _configureAction?.Invoke(options);
+
+            ValidateOptions(options);
 
             return new LetsEncrypt(options) {
                 ConfigureHandler = _configureHandler,
                 ErrorHandler = _errorHandler,
                 ContinueHandler = _continueHandler
             };
+        }
+
+        private static void ValidateOptions(LetsEncryptOptions options)
+        {
+            options.ArgNotNull(nameof(options));
+
+            // Validate options
+            options.Hostname.OptionNotBlank(nameof(LetsEncryptOptions.Hostname));
+            options.Email.OptionNotBlank(nameof(LetsEncryptOptions.Email));
+
+            // Validate authority options
+            options.Authority?.Name.OptionNotBlank($"{nameof(LetsEncryptOptions.Authority)}.{nameof(Authority.Name)}");
+            options.Authority?.DirectoryUri.OptionNotBlank($"{nameof(LetsEncryptOptions.Authority)}.{nameof(Authority.DirectoryUri)}");
+
+            // Validate certificate options
+            options.Certificate?.Filename.OptionNotBlank($"{nameof(LetsEncryptOptions.Certificate)}.{nameof(Certificate.Filename)}");
+            options.Certificate?.FriendlyName.OptionNotNull($"{nameof(LetsEncryptOptions.Certificate)}.{nameof(Certificate.FriendlyName)}");
+            options.Certificate?.Password.OptionNotNull($"{nameof(LetsEncryptOptions.Certificate)}.{nameof(Certificate.Password)}");
+
+            // Validate certificate signing request (CSR) options
+            options.CsrInfo?.CountryName.OptionNotNull($"{nameof(LetsEncryptOptions.CsrInfo)}.{nameof(CsrInfo.CountryName)}");
+            options.CsrInfo?.State.OptionNotNull($"{nameof(LetsEncryptOptions.CsrInfo)}.{nameof(CsrInfo.State)}");
+            options.CsrInfo?.Locality.OptionNotNull($"{nameof(LetsEncryptOptions.CsrInfo)}.{nameof(CsrInfo.Locality)}");
+            options.CsrInfo?.Organization.OptionNotNull($"{nameof(LetsEncryptOptions.CsrInfo)}.{nameof(CsrInfo.Organization)}");
+            options.CsrInfo?.OrganizationUnit.OptionNotNull($"{nameof(LetsEncryptOptions.CsrInfo)}.{nameof(CsrInfo.OrganizationUnit)}");
         }
     }
 }
