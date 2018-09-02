f43587a08814d15081484a34598a522f3b567cdc
Bruno Zell <bruno.zzell@gmail.com>
2018-09-02 15:03:36 +0200

    remove startup from example

--- example/WebApp/Program.cs
+++ example/WebApp/Program.cs
@@ -1,7 +1,10 @@
-﻿using Microsoft.AspNetCore;
-using Microsoft.AspNetCore.Hosting;
-using AspNetCore.LetsEncrypt;
+﻿using AspNetCore.LetsEncrypt;
 using AspNetCore.LetsEncrypt.Options;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace WebApp {
     public class Program {
@@ -15,11 +18,16 @@ namespace WebApp {
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args, Certificate httpsCertificate) =>
-            WebHost.CreateDefaultBuilder<Startup>(args)
-                .UseUrls() // Remove warning for overridden settings by UseKestrel
+            WebHost.CreateDefaultBuilder(args)
+                .UseUrls() // Remove warning for overridden settings by UseKestrel.
                 .UseKestrel(options => {
                     options.ListenAnyIP(80);
                     options.ListenAnyIP(443, o => o.UseHttps(httpsCertificate.Filename, httpsCertificate.Password));
+                })
+                .Configure(app => {
+                    // Return "Hello World!" on every request.
+                    // For simplicity we don't use a Startup-class in this example.
+                    app.Run(context => context.Response.WriteAsync("Hello World!"));
                 });
     }
 }
