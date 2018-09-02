ecf7372486bcdb4927be063c4b41e46aa5fc5f19
Bruno Zell <bruno.zzell@gmail.com>
2018-09-02 22:48:11 +0200

    explaning comments

--- example/WebApp/Program.cs
+++ example/WebApp/Program.cs
@@ -10,33 +10,37 @@ using System;
 
 namespace WebApp {
     public static class Program {
-        public static void Main(string[] args)
-        {
-            var configuration = BuildConfiguration(args);
-
+        public static void Main(string[] args) =>
             new LetsEncryptBuilder()
-                .WithConfiguration(configuration.GetSection("LetsEncrypt"))
-                .WithOptions(o => {
-                    // Overwrite some options provided by WithConfiguration
-                    //o.Hostname = "";
+                .WithConfiguration(BuildConfiguration(args).GetSection("LetsEncrypt")) // See appsettings.json for structure
+                .WithOptions(options => {
+                    // Here you can overwrite some options provided by WithConfiguration(..).
+                    // Or don't use WithConfiguration(..) at all and configure everything in code here.
+                    options.Hostname = "hostname.com";
                 })
                 .ConfigureWebHost(builder => {
+                    // For the ACME chellenge a small Kestrel-server is hosted before the actual web app starts up.
+                    // For advanced usage scenarios you have access to the IWebHostBuilder
                 })
-                .OnError(o => {
-                    o.Continue = false;
-                    Console.WriteLine(o.Exception.Message);
+                .OnError(options => {
+                    // Get more information about the error
+                    Console.WriteLine(options.Exception.InnerException.Message);
+
+                    // Whether to start the web app configured in ContinueWith(..) anyways
+                    options.Continue = false;
                 })
                 .ContinueWith(cert => CreateWebHostBuilder(args, cert).Build())
                 .Build()
                 .Run(); // OR .RunAsync()
-        }
 
-        private static IWebHostBuilder CreateWebHostBuilder(string[] args, Certificate sslCertificate) =>
+        private static IWebHostBuilder CreateWebHostBuilder(string[] args, Certificate certificate) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseUrls() // Removes warning for overridden settings by UseKestrel.
                 .UseKestrel(options => {
+                    // Listen to http traffic as wall as https traffice with the generated certificate.
+                    // By now the certificate is stored in a pfx-file under the configured path.
                     options.ListenAnyIP(80);
-                    options.ListenAnyIP(443, o => o.UseHttps(sslCertificate.Filename, sslCertificate.Password));
+                    options.ListenAnyIP(443, o => o.UseHttps(certificate.Filename, certificate.Password));
                 })
                 .Configure(app => {
                     // Return "Hello World!" on every request.
